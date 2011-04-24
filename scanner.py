83a0b3a844b7140ea3e96d450943fe73e8c16710
Steve Gricci <steve@gricci.org>
2011-04-24 14:42:25 -0600

    Added TODO file Modularized the scanner.py functions: 	scan_port(ip,port) [returns bool] 	is_host_up(ip) [returns bool] (Uses subprocess and ping) Next step will be to increase understanding of ips (like nmap) and thread it

--- scanner.py
+++ scanner.py
@@ -8,28 +8,50 @@
 
 from socket import *
 import sys
+import subprocess
 from Con import *
 from progress import ProgressBar
-if __name__ == '__main__':  
-	# Check that an IP/Hostname was sent
+
+def scan_port(ip, port):
+	s = socket(AF_INET, SOCK_STREAM)
+	result = s.connect_ex((ip, port))
+	s.close()
+	if (result == 0):
+		return True
+	return False
+
+def usage():
 	if (len(sys.argv) != 2):
 		sys.stderr.write('Usage: scanner.py <ip>')
 		sys.exit(1)
+	return True
 
+def is_host_up(ip):
+	ret = subprocess.call("ping -c 1 -t 1 %s" % ip,
+			shell=True, stdout=open('/dev/null'),
+			stderr=subprocess.STDOUT)
+	if (ret == 0):
+		return True
+	return False
+
+if __name__ == '__main__':  
+	# Check that an IP/Hostname was sent
+	usage()
 	# Load Configuration
 	con = Con('ports.ini');
-
 	targetIP = gethostbyname(sys.argv[1])
+
+	# Check if the host is up first
+	if (is_host_up(targetIP) == False):
+		print "DOWN: %s is down" % targetIP
+		sys.exit(1)
+
 	print 'Starting scan on host: ', targetIP
 	p = ProgressBar(1024)
 	open_ports = {}
 	for i in range(1, 1024):
 		p.update_time(i)
-		s = socket(AF_INET, SOCK_STREAM)
-		result = s.connect_ex((targetIP,i))
-		s.close()
-		if(result == 0):
+		if(scan_port(targetIP, i)):
 			open_ports[str(i).rjust(5, '0')] = con.get(i);
-		s.close()
 	for port in sorted(open_ports.iterkeys()):
 		print "Port (%s): %s" % (str(int(port)).rjust(5, ' '), open_ports[port])
