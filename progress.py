07f1574387ffafd35434e812a0006e9c5b9d4b94
Steve Gricci <steve@gricci.org>
2011-04-23 22:55:47 -0600

    Removed sample code from progress.py

--- progress.py
+++ progress.py
@@ -9,14 +9,10 @@
 import sys
 import time
 
-
-
 if sys.platform.lower().startswith('win'):
     on_windows = True
 else:
     on_windows = False
-
-
 
 class ProgressBar:
     def __init__(self, duration):
@@ -35,12 +31,12 @@ class ProgressBar:
             self.update_time(i + 1)
             time.sleep(1) 
         print self
-        
+
     def update_time(self, elapsed_secs):
         print self, chr(27) + '[A'
         self.__update_amount((elapsed_secs / float(self.duration)) * 100.0)
         self.prog_bar += '  %d/%s' % (elapsed_secs, self.duration)
-        
+
     def __update_amount(self, new_amount):
         percent_done = int(round((new_amount / 100.0) * 100.0))
         all_full = self.width - 2
@@ -50,73 +46,6 @@ class ProgressBar:
         pct_string = '%d%%' % percent_done
         self.prog_bar = self.prog_bar[0:pct_place] + \
             (pct_string + self.prog_bar[pct_place + len(pct_string):])
-        
+
     def __str__(self):
         return str(self.prog_bar)
-        
-        
-
-
-if __name__ == '__main__':
-    """ example usage """   
-    
-    # print a static progress bar
-    #  [##########       25%                  ]  15s/60s
-
-    p = ProgressBar(60)
-    p.update_time(15)
-    print '\nstatic progress bar:'
-    print p
-    
-    
-    # print a static progress bar
-    #  [=================83%============      ]  25s/30s
-
-    p = ProgressBar(30)
-    p.fill_char = '='
-    p.update_time(25)
-    print '\nstatic progress bar:'
-    print p
-    
-    
-    # print a dynamic updating progress bar on one line:
-    #
-    #  [################100%##################]  10s/10s
-    #  done
-    
-    secs = 10
-    p = ProgressBar(secs)
-    print '\n\ndynamic updating progress bar:'
-    print '\nplease wait %d seconds...\n' % secs
-    
-    # spawn asych (threads/processes/etc) code here that runs for secs.
-    # the call to .animate() blocks the main thread.
-    
-    p.animate(secs)
-    
-    print 'done'
-
-
-
-"""
-example output:
-
-
-$ python progress_bar.py 
-
-static progress bar:
-[##########       25%                  ]  15s/60s
-
-static progress bar:
-[=================83%============      ]  25s/30s
-
-
-dynamic updating progress bar:
-
-please wait 10 seconds...
-
-[################100%##################]  10s/10s
-done
-
-"""
-
